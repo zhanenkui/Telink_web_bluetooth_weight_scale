1726bd3847f09fa2a5a77ce363ad725ef6ecfa23
engkui.zhan <engkui.zhan@telink-semi.com>
2020-10-30 19:54:01 +0800

    first commit file

---
+++ telink-weight.js
@@ -0,0 +1,63 @@
+class Fanlight {
+
+  constructor() {
+    this.thedevice = null;
+    this.gattserve = null;
+    this.Txchartic = null;
+    this.onDisconnected = this.onDisconnected.bind(this);
+  }
+
+  /**
+   * @requires
+   * Scan the nearby Bluetooth and click the connection
+   */
+  request() {
+    let options = {
+      acceptAllDevices: true,
+      optionalServices: ["0000181d-0000-1000-8000-00805f9b34fb"]
+      };
+    return navigator.bluetooth.requestDevice(options)
+    .then(device => {
+      this.thedevice = device;
+      addLoginfo('Name:             ' + device.name);
+      addLoginfo('Id:               ' + device.id);
+      addLoginfo('Connected:        ' + device.gatt.connected);
+      device.addEventListener('gattserverdisconnected', this.onDisconnected)
+      return device.gatt.connect();
+    })
+    .then(server => {
+      addLoginfo('Getting Service...');
+      this.gattserve = server;
+      return server.getPrimaryService("0000181d-0000-1000-8000-00805f9b34fb");
+    })
+    .then(service => {
+      addLoginfo('Getting Characteristic...');
+      addLoginfo('service UUID:              ' + service.uuid);
+      return service.getCharacteristic("00002a9d-0000-1000-8000-00805f9b34fb");
+    })
+    .then(characteristic => {
+      this.Txchartic = characteristic;
+      addLoginfo('write Characteristic UUID: ' + characteristic.uuid);
+      Data_info_manage(this.gattserve);
+    });
+  }
+
+  /**
+   * @disconnect
+   * Disconnect from the device
+   */
+  disconnect() {
+    if (!this.thedevice) {
+      return Promise.reject('Device is not connected.');
+    }
+    return this.thedevice.gatt.disconnect();
+  }
+
+  /**
+   * @onDisconnected
+   * Bluetooth device disconnect execution function
+   */
+  onDisconnected() {
+    addLoginfo('Device is disconnected.');
+  }
+}
